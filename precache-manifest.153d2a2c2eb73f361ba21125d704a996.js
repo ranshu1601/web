33a56cd33dd20136f42e6bb9f8e341d3e0bb16be
Anshu Ranjan <ranshu1601@gmail.com>
2022-08-21 15:24:54 +0530

    Updates

---
+++ precache-manifest.153d2a2c2eb73f361ba21125d704a996.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6560b7dfcd7b6729476",
+    "url": "/web/static/css/main.1c5e6156.chunk.css"
+  },
+  {
+    "revision": "d6560b7dfcd7b6729476",
+    "url": "/web/static/js/main.d6560b7d.chunk.js"
+  },
+  {
+    "revision": "5697770ea0c8b19522fc",
+    "url": "/web/static/css/1.0f546280.chunk.css"
+  },
+  {
+    "revision": "5697770ea0c8b19522fc",
+    "url": "/web/static/js/1.5697770e.chunk.js"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/web/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "b1e81d847e8ad09ad1f8624e768752f2",
+    "url": "/web/static/media/me.b1e81d84.png"
+  },
+  {
+    "revision": "b4890ab7e474a1d2a33bbc0f85c8fa83",
+    "url": "/web/static/media/me-about.b4890ab7.jpg"
+  },
+  {
+    "revision": "e37bce5e5fb807ef56ee1862f6d16834",
+    "url": "/web/static/media/portfolio1.e37bce5e.jpg"
+  },
+  {
+    "revision": "4a6488875dfdda2055563c82b8928fbc",
+    "url": "/web/static/media/portfolio2.4a648887.jpg"
+  },
+  {
+    "revision": "f9ce28df42b35ef85a1bc1527c3ab921",
+    "url": "/web/static/media/portfolio3.f9ce28df.jpg"
+  },
+  {
+    "revision": "29e4c53ae7f8ec1a9a40b71845867747",
+    "url": "/web/static/media/portfolio4.29e4c53a.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/web/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/web/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "e3888081d02b819380e0c38a3d2c338b",
+    "url": "/web/index.html"
+  }
+];
